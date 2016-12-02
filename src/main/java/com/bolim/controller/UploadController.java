e680da8e59d6d73a7aeb62ff1648e802a5068707
babooteng2 <babooteng2@gmail.com>
2016-12-03 02:38:13 +0900

    전송된 파일을 화면에 표시하기( 컨트롤러 )

--- src/main/java/com/bolim/controller/UploadController.java
+++ src/main/java/com/bolim/controller/UploadController.java
@@ -2,13 +2,18 @@ package com.bolim.controller;
 
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.UUID;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.bolim.util.MediaUtils;
 import com.bolim.util.UploadFileUtils;
 
 @Controller
@@ -64,5 +70,37 @@ public class UploadController {
 						file.getOriginalFilename(),
 						file.getBytes()),
 				HttpStatus.CREATED);
+	}
+	
+	@ResponseBody
+	@RequestMapping("/displayFile")
+	public ResponseEntity<byte[]> displayFile(String fileName) throws Exception {
+		InputStream in = null;
+		ResponseEntity<byte[]> entity = null;
+		logger.info("fileName : " + fileName);
+		try{
+			String formatName = fileName.substring(fileName.lastIndexOf(".")+1);
+			MediaType mType = MediaUtils.getMediaType(formatName);
+			HttpHeaders headers = new HttpHeaders();
+			in = new FileInputStream(uploadPath + fileName);
+			if( mType != null ) {
+				headers.setContentType(mType);
+			}else{
+				fileName = fileName.substring(fileName.indexOf("_")+1);
+				// 다운로드용 마임타입
+				headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+				headers.add("Content-Disposition", "attachment; filename=\""+
+							new String(fileName.getBytes("UTF-8"), "ISO-8859-1") + "\""); 
+			}
+			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in),
+						headers,
+						HttpStatus.CREATED);
+		}catch(Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+		}finally{
+			in.close();
+		}
+		return entity;
 	}
 }
